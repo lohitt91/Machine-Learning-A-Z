a537f20d49c2ecca99ba5d9c8e289ed85abddd1e
Lohitt <lohitt91@gmail.com>
2018-03-11 18:46:21 +1100

    Categorical data enoder script added

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Mar 10 20:17:00 2018
 
-@author: lohit
+@author: lohitt
 """
 
 # Data Preprocessing Template
@@ -13,4 +13,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Creating the dataset
 dataset = pd.read_csv("Data.csv")
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+# Processing missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = "NaN", strategy="mean", axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
