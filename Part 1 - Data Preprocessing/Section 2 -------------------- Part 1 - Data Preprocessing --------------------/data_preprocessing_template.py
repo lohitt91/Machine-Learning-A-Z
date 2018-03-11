2bd7e1ebf4fe08c3622025bf3b7d59c47db81625
Lohitt <lohitt91@gmail.com>
2018-03-11 17:59:30 +1100

    Initial commit

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 10 20:17:00 2018
+
+@author: lohit
+"""
+
+# Data Preprocessing Template
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Data.csv")
